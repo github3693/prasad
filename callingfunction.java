98bb148bcbc90fe6261201f682c02885c7db433e
github3693 <105473229+github3693@users.noreply.github.com>
2022-05-16 00:20:29 -0700

    Add files via upload

---
+++ callingfunction.java
@@ -0,0 +1,23 @@
+package package1;
+
+ class  Main {
+	 
+ 	public int performAddition(int num1,int num2) {
+ 		return  num1+num2;
+ 					
+	
+ }   
+ 	public static void main(String[] args) {
+	      
+	    Main main= new Main ();
+	    
+	    int num1=2;
+	    int num2=3;
+
+	    
+         main.performAddition( num1,num2);
+    
+  }
+
+
+}
